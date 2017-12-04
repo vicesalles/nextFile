7115ec16f899c5b8d162a5100e49767fd6f0d68f
Vice Sallés <vicenc.salles@gmail.com>
2017-12-04 17:21:01 +0100

    feat: more cameras

---
+++ filenameSamples.js
@@ -0,0 +1,48 @@
+/*
+EMPTY CÀMERA
+
+{
+    name:'',
+    file:'',
+    solution:''
+}
+
+/*
+DJI MAVIC PRO: DJI_0121.MOV
+GOPRO: GOPR9802.MP4
+CANON: MVI_0863.MP4
+OLYMPUS:
+
+*/
+
+
+
+const filenames = [
+    {
+        name:'GOPRO',
+        file:'GOPR9802',
+        solution:'GOPR9803'
+    },
+    {
+        name:'GOPRO',
+        file:'GOPR0099',
+        solution:'GOPR0100'
+    },
+    {
+        name:'DJI Mavic Pro',
+        file:'DJI_0121',
+        solution:'DJI_0122'
+    },
+    {
+        name:'DJI Mavic Pro',
+        file:'DJI_1021',
+        solution:'DJI_1022'
+    },
+    {
+        name:'CANON XA30',
+        file:'MVI_0863',
+        solution:'MVI_0864'
+    }
+]
+
+module.exports = filenames;
